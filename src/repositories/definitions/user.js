f2bc70bcb58d8930d75bfce53faaae9ade0426d3
MinJun Seo <smj0385@naver.com>
2020-09-05 13:19:30 +0900

    Implement user class
    database의 user 테이블에 해당하는 class를 생성하였습니다.

---
+++ src/repositories/definitions/user.js
@@ -0,0 +1,34 @@
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('../../loaders/database');
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING(10),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(254),
+    allowNull: false,
+    unique: true,
+    // email 양식이 맞는지 검사해주는 option
+    validate: {
+      isEmail: true
+    }
+  },
+  pw: {
+    type: DataTypes.STRING(33),
+    allowNull: false
+  }
+}, {
+  sequelize,
+  tableName: 'User'
+});
+
+module.exports = User;
