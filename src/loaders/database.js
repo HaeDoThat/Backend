6e827f251339601841071afe9b280204ca60c30b
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-07 19:57:07 +0900

    [FIX] typo

--- src/loaders/database.js
+++ src/loaders/database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DBNAME } = require('../configs');
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DBNAME } = require('../config');
 
 // connecting to a database
 const sequelize = new Sequelize(DB_DBNAME, DB_USER, DB_PASSWORD, {
