c75ceb291f019ef9d6b6a1b2e5e1f527a2001072
MinJun Seo <smj0385@naver.com>
2020-09-07 17:25:30 +0900

    Implement index.js
    두 테이블 간의 관계를 명시합니다.

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,1 +1,17 @@
+const { User, UserAccessToken } = require('./definitions');
 
+User.hasMany(UserAccessToken, {
+  foreignKey: 'user_id',
+  sourceKey: 'id',
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE'
+});
+
+UserAccessToken.belongsTo(User, {
+  foreignKey: 'user_id',
+  targetKey: 'id',
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, UserAccessToken };
