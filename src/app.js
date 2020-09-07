9b897a0b324b54b60548ad1e5eb51e59b31420ba
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-07 18:56:05 +0900

    [ADD] implement app entry point

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const init_app = require('./loaders');
+const configs = require('./config');
+
+const app = express();
+const PORT = configs.SERVER_PORT;
+
+
+init_app(app);
+
+
+app.listen(PORT, () => {
+    console.log(`start the server at ${PORT}`);
+});
 
