f0c49b691bd216f4053d4a3a47b99dadfcbc72a3
MinJun Seo <smj0385@naver.com>
2020-09-07 17:31:46 +0900

    Add connection the database
    database에 연결을 시도하는 connection 코드 추가

---
+++ src/loaders/database.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize');
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DBNAME } = require('../configs');
+
+// connecting to a database
+const sequelize = new Sequelize(DB_DBNAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'mysql',
+  define: {
+    freezeTableName: true,
+    timestamps: false
+  }
+});
+
+// test the connection
+async function connectDatabase() {
+  try {
+    await sequelize.authenticate();
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  sequelize,
+  connectDatabase
+}
