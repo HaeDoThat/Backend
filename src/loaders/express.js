4bb77db4cbdee6373369adb0f4fee566171ab7eb
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-07 20:09:36 +0900

    [ADD] bind routes

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
+const routes = require('../routes');
+
 module.exports = (app) => {
   app.use(cors);
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  app.use('/', routes);
+
+  app.use()
 };
