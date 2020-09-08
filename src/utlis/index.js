bd1c730f442a183489c85ed3ced1cf7b8bfd87b2
MinJunSeo <smj0385@naver.com>
2020-09-08 18:56:45 +0900

    Add isPassword util export

--- src/utlis/index.js
+++ src/utlis/index.js
@@ -1,9 +1,11 @@
 const isRegisteredEmail = require('./isRegisterdEmail');
 const { createHash } = require('./createHash');
 const isName = require('./isName');
+const isPassword = require('./isPassword');
 
 module.exports = {
   isRegisteredEmail,
   createHash,
-  isName
+  isName,
+  isPassword
 };
