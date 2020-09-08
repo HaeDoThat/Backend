fff2b37f8fb64100d42194b2cee3ec22e168c860
MinJunSeo <smj0385@naver.com>
2020-09-08 19:04:35 +0900

    Fix exports for using isEmail util

--- src/utlis/index.js
+++ src/utlis/index.js
@@ -2,10 +2,12 @@ const isRegisteredEmail = require('./isRegisterdEmail');
 const { createHash } = require('./createHash');
 const isName = require('./isName');
 const isPassword = require('./isPassword');
+const isEmail = require('./isEmail');
 
 module.exports = {
   isRegisteredEmail,
   createHash,
   isName,
-  isPassword
+  isPassword,
+  isEmail
 };
