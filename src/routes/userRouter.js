e3c244bbb461bf3707e7a54dde1a3b677b7f9413
MinJunSeo <smj0385@naver.com>
2020-09-08 20:11:36 +0900

    Fix using middleware

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,13 +4,14 @@ const { User } = require('../repositories');
 const AuthUser = require('../services/authUser');
 const authUser = new AuthUser(User);
 
+const { isEmail, isName, isPassword } = require('./middleware');
 const { isRegisteredEmail, createHash } = require('../utlis');
 
 const router = express.Router();
 
 // login 되어 있지 않은지 확인하는 middlewrae 필요
 // 각 입력 형식이 적절한지 확인 필요
-router.post('/', async (req, res, next) => {
+router.post('/', isEmail, isName, isPassword, async (req, res, next) => {
   const { name, email, password } = req.body;
   try {
     if (isRegisteredEmail(User, email)) {
@@ -19,7 +20,7 @@ router.post('/', async (req, res, next) => {
       });
     }
 
-    authUser.singUp(name, email, password);
+    authUser.signUp(name, email, password);
     
     res.status(201).send();
   } catch (error) {
