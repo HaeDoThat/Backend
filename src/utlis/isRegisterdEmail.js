53e16ee5d7591fee9bf9b797e0d04cf7ca5d5ed6
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-08 17:06:16 +0900

    [FIX] local valiable is changed email to finded_email

--- src/utlis/isRegisterdEmail.js
+++ src/utlis/isRegisterdEmail.js
@@ -1,9 +1,9 @@
 async function isRegisteredEmail(userModel, email) {
-  const email = await userModel.findOne({
+  const finded_email = await userModel.findOne({
     attributes: ['email'],
     where: { email: email }
   });
-  if (email) {
+  if (finded_email) {
     return true;
   }
   return false;
