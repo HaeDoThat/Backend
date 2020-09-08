b02ff95e092e25cfc3b4ecff6e40f26908da4ec1
MinJunSeo <smj0385@naver.com>
2020-09-08 20:12:00 +0900

    Fix cors() it is a function

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const routes = require('../routes');
 
 module.exports = (app) => {
-  app.use(cors);
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-
+  
   app.use('/', routes);
 };
