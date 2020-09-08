3fd16b5c5de149fe47b155496be3e0d620b499b0
MinJunSeo <smj0385@naver.com>
2020-09-08 17:13:07 +0900

    Add check alright name form util

---
+++ src/utlis/isName.js
@@ -0,0 +1,18 @@
+const { badRequest, invalidName } = require('../error/error');
+
+function isName(name) {
+  const checkRegExp = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|a-z|A-Z|0-9||_]|-$/;
+
+  if (!name) {
+    throw badRequest;
+  }
+  if (name.length < 2 || name.length > 10) {
+    throw invalidName;
+  }
+  if (!checkRegExp.test(name)) {
+    throw invalidName;
+  }
+  return true;
+}
+
+module.exports = isName;
