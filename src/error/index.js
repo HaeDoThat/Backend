0a8513a78fed35ac18f5d2950c589baed8015811
MinJunSeo <smj0385@naver.com>
2020-09-08 20:09:32 +0900

    Rename error.js to index.js

--- src/error/index.js
+++ src/error/index.js
@@ -7,11 +7,13 @@ class HttpError extends Error {
 }
 
 const badRequest = new HttpError(400, '올바르지 않은 요청입니다.');
+const invalidEmail = new HttpError(412, 'email의 형식이 올바르지 않습니다.', 'email');
 const invalidName = new HttpError(412, 'name의 형식이 올바르지 않습니다.', 'name');
 const invalidPassword = new HttpError(412, 'password의 형식이 올바르지 않습니다.', 'password');
 
 module.exports = {
   badRequest,
+  invalidEmail,
   invalidName,
   invalidPassword
 };
