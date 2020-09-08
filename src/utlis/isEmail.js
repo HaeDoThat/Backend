f54b5c743f89fac93923f6a788124dbfd458fb92
MinJunSeo <smj0385@naver.com>
2020-09-08 20:10:34 +0900

    Fix using error module

--- src/utlis/isEmail.js
+++ src/utlis/isEmail.js
@@ -1,7 +1,14 @@
 const validator = require('validator');
+const { badRequest, invalidEmail } = require('../error');
 
 function isEmail(email) {
-  return validator.isEmail(email);
+  if(validator.isEmail(email)) {
+    return true;
+  } else if(!email) {
+    throw badRequest;
+  } else {  // !validator.isEmail(email)
+    throw invalidEmail;
+  }
 }
 
 module.exports = isEmail;
