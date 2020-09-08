04307ceb24db6d38d3f8daefa4c84c0a93701129
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-08 19:49:22 +0900

    [ADD] compareDataAndHash

--- src/utlis/index.js
+++ src/utlis/index.js
@@ -1,10 +1,12 @@
 const isRegisteredEmail = require('./isRegisterdEmail');
 const { createHash } = require('./createHash');
+const compareDataAndHash = require('./compareDataAndHash');
 const isName = require('./isName');
 const isPassword = require('./isPassword');
 
 module.exports = {
   isRegisteredEmail,
+  compareDataAndHash,
   createHash,
   isName,
   isPassword
