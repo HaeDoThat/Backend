8530e569ee132f12b59d7e528f70c95064519336
MinJunSeo <smj0385@naver.com>
2020-09-09 09:25:41 +0900

    Fix more validate terms

--- src/utlis/isPassword.js
+++ src/utlis/isPassword.js
@@ -1,10 +1,14 @@
 const { badRequest, invalidPassword } = require('../error');
 
 function isPassword(password) {
+  const checkRegExp = /[0-9|A-Z|a-z|\{|\}|\[|\]|\/|?|.|,|;|:|\||\)|\_|*|~|`|!|^|\-|+|<|>|@|\#|$|%|&|\\|\=|\(|\'|\"|]/g;
   if (!password) {
     throw badRequest;
   }
-  if (password.length < 8) {
+  if (password.length < 8 || password.length > 20) {
+    throw invalidPassword;
+  }
+  if (!checkRegExp.test(password)) {
     throw invalidPassword;
   }
   return true;
