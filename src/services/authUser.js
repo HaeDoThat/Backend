0b84a88b80e5d7f59a228c20d06c6c37af63e5b2
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-23 15:42:58 +0900

    [FIX] typo

--- src/services/authUser.js
+++ src/services/authUser.js
@@ -37,8 +37,8 @@ class AuthUser {
 
         const stored_user = user_entity.dataValues;
 
-        const flag_password_valid = await compareDataAndHash(password, stored_user.password);
-        console.log(flag_password_valid);
+        const flag_password_valid = await compareDataAndHash(password, stored_user.pw);
+        
         if (flag_password_valid == false) {
             throw new Excpetions.FailtoAuthenticationExcpetion;
         }
